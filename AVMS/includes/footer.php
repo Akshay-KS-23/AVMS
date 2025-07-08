30110d72ba2d5ff014c41014ace57d5441cdae24
Akshay Kumar Singh <akshayks2332@gmail.com>
2025-07-08 22:36:03 +0530

    Initial commit with project folders and files

---
+++ AVMS/includes/footer.php
@@ -0,0 +1,7 @@
+<div class="row">
+                            <div class="col-md-12">
+                                <div class="copyright">
+                                    <b><p>Apartment Visitors Management System</p></b>
+                                </div>
+                            </div>
+                        </div>
